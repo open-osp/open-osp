1c6a2efee9042f610f9dbd5d1fd055b7a6604ad1
Clark Van Oyen <clark@countable.ca>
2019-10-31 17:08:51 -0700

    modify to use Aria

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ else
 fi
 
 echo "Compiling OSCAR. This may take some time...."
-docker-compose run builder
+#docker-compose run builder
 echo "Setting up docker containers. This may take some time...."
 docker-compose up -d db
 echo "Waiting for db containers initialize (1 min)"
@@ -23,5 +23,4 @@ echo "Bringing up tomcat"
 docker-compose up -d tomcat_oscar
 
 echo "OSCAR is set up at http://localhost:8091/oscar_mcmaster"
-echo "This script is a fork of "Oscar in a Box" by http://nuchange.ca"
-
+echo "This script is a fork of 'Oscar in a Box' by http://nuchange.ca"
