09deb595dfae1c36f708c3b55ce17f6a920da5ed
Clark Van Oyen <clark@countable.ca>
2019-11-13 12:53:07 -0800

    fix missing export

--- deploy-source.sh
+++ deploy-source.sh
@@ -7,7 +7,7 @@ echo "This script is a fork of "Oscar in a Box" by http://nuchange.ca"
 echo "Destroy any old instance (keeping database)."
 docker-compose down
 
-OSCAR_WAR=./oscar/target/oscar-14.0.0-SNAPSHOT.war
+export OSCAR_WAR=./oscar/target/oscar-14.0.0-SNAPSHOT.war
 
 echo "Compiling OSCAR. This may take some time...."
 docker-compose run builder ./bin/build.sh
