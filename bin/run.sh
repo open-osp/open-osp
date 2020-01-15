d1d80293900f4c3ae2f4fa33c6a6558ba7a46b01
Clark Van Oyen <clark@countable.ca>
2020-01-15 14:53:47 -0800

    small tweaks

--- bin/run.sh
+++ bin/run.sh
@@ -8,6 +8,7 @@ docker-compose exec db ./bin/populate-db.sh
 sleep 10
 echo "Bringing up tomcat"
 docker-compose up -d tomcat_oscar
+docker-compose up -d nginx
 
 echo "OSCAR is set up at http://localhost/oscar_mcmaster"
 echo "OpenOSP started as a fork of "Oscar in a Box" by http://nuchange.ca"
