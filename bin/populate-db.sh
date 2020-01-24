7aa2d335c08e51c07261eefee3d949f147e3959d
ferdinandpolpol <ferdinand@countable.ca>
2020-01-24 11:58:29 +0800

    update default expiry

--- bin/populate-db.sh
+++ bin/populate-db.sh
@@ -9,3 +9,8 @@ done
 cd oscar/database/mysql
 ./createdatabase_bc.sh oscar oscar oscar_mcmaster
 
+echo "Disabling default user expiry..."
+mysql -uroot -pliyi oscar_mcmaster << QUERY
+update security set date_ExpireDate=DATE_ADD(CURDATE(), INTERVAL 360 MONTH), b_ExpireSet=1 
+where user_name='oscardoc';
+QUERY
