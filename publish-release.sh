61c7b0a8bfe3b450133e54c14dc7b3dd0be36505
Clark Van Oyen <clark@countable.ca>
2020-02-26 15:12:28 -0800

    publish script, dockerhub hook

---
+++ publish-release.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#TODO, make it more clear this job clears the local DB
+#./purge.sh
+#./build-release.sh
+
+VER=$(date +"%Y.%m.%d")
+docker tag openosp/open-osp:latest openosp/open-osp:$VER
+docker push openosp/open-osp:$VER
+docker push openosp/open-osp:latest
+
