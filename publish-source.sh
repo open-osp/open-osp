ac2054fd0dbac50a6f573506e195880831922ab2
Clark Van Oyen <clark@countable.ca>
2020-02-26 18:58:02 -0800

    publish source login

--- publish-source.sh
+++ publish-source.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 #TODO, make it more clear this job clears the local DB
-#./purge.sh
-#./build-source.sh
+./purge.sh
+./build-source.sh
 
 VER=$(date +"%Y.%m.%d")
 docker tag openosp/open-osp:latest openosp/open-osp:$VER
+docker login --username=$DOCKERHUB_USERNAME --password=$DOCKERHUB_PASSWORD
 docker push openosp/open-osp:$VER
 docker push openosp/open-osp:latest
 
