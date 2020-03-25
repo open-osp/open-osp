b30c4c365a6c3b54b8521ff14873ca314db1d0fb
Colcamex Resources Inc <dwarren@colcamex.com>
2020-03-24 21:30:36 -0700

    More settings.

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #echo "Setting up database..."
-#docker-compose up -d db
-#sleep 10
+docker-compose up -d db
+sleep 10
 
 echo "Generating self-signed cert for temporary use. Please replace with a CA signed one."
 openssl req -new -newkey rsa:4096 -days 3650 -nodes -x509 -subj "/C=CA/ST=BC/L=Vancouver/O=OpenOSP/CN=${HOST:-openosp.ca}" -keyout ./volumes/ssl.key -out ./volumes/ssl.crt
--- bin/start.sh
+++ bin/start.sh
@@ -3,7 +3,12 @@
 docker-compose stop tomcat_oscar
 docker-compose rm tomcat_oscar
 
+docker-compose stop tomcat_expedius
+docker-compose rm tomcat_expedius
+
 docker-compose up -d db
 sleep 5
+
 docker-compose up -d tomcat_oscar
 
+docker-compose up -d tomcat_expedius
