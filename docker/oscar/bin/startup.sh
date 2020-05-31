f3dbad5604b32d292fc70c9ed21d36085fd05754
ferdinandpolpol <ferdinand.polpol@gmail.com>
2020-05-31 12:37:38 -0700

    update drugref2

---
+++ docker/oscar/bin/startup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -euxo
+
+catalina.sh start
+
+rm -rf /usr/local/tomcat/webapps/ROOT
+rm -rf /usr/local/tomcat/webapps/docs
+rm -rf /usr/local/tomcat/webapps/examples
+rm -rf /usr/local/tomcat/webapps/manager
+rm -rf /usr/local/tomcat/webapps/host-manager
+
+tail -f /usr/local/tomcat/logs/catalina.out
+
+echo "Updating drugref"
+curl -v -H "content-type:text/xml" \ 
+    http://oscar:8080/drugref2/DrugrefService --data @bin/drugref2/updatedrugref.xml
