9102b64ce6460b87d25fa554384130286330d718
ferdinandpolpol <ferdinand.polpol@gmail.com>
2020-06-06 12:16:05 -0700

    do not delete ROOT and add drugref scripts through Dockerfile

--- docker/oscar/bin/startup.sh
+++ docker/oscar/bin/startup.sh
@@ -4,7 +4,6 @@ set -euxo
 
 catalina.sh start
 
-rm -rf /usr/local/tomcat/webapps/ROOT
 rm -rf /usr/local/tomcat/webapps/docs
 rm -rf /usr/local/tomcat/webapps/examples
 rm -rf /usr/local/tomcat/webapps/manager
