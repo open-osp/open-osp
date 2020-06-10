90384f5f6e070616af52a4ec9a3d811ad99a4ccf
ferdinandpolpol <ferdinand.polpol@gmail.com>
2020-06-10 08:37:36 -0700

    add -f for openosp start

--- bin/openosp-start.sh
+++ bin/openosp-start.sh
@@ -3,10 +3,10 @@
 echo "Starting OpenOSP services."
 
 docker-compose stop oscar
-docker-compose rm oscar
+docker-compose rm -f oscar
 
 docker-compose stop expedius
-docker-compose rm expedius
+docker-compose rm -f expedius
 
 docker-compose up -d db
 sleep 5
