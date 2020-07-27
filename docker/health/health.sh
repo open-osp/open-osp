881bca0c59ff6a5d168d7b13f17dc87b911c2f1d
Clark <clark@countable.ca>
2020-07-26 22:23:17 -0700

    health script to use proper cert

--- docker/health/health.sh
+++ docker/health/health.sh
@@ -7,12 +7,13 @@ ping -c 1 expedius
 ping -c 1 db
 ping -c 1 oscar
 
-faxws_http_code=`curl -s -o /dev/null -w "%{http_code}" -Ik -u faxws:$FAXWS_TOMCAT_PASSWORD https://faxws/faxWs`
 
-if [ -z "$FAXWS_TOMCAT_PASSWORD" ]; then
+if [ -z ${FAXWS_TOMCAT_PASSWORD+x} ]; then
     echo "skipping faxws, not defined."
 else
     faxws_allow=`curl -Ik -u faxws:$FAXWS_TOMCAT_PASSWORD https://faxws/faxWs 2>/dev/null | grep "Allow" | head -1 | cut -d":" -f2`
+    openssl x509 -in /usr/local/tomcat/conf/ssl.crt -out /tmp/ssl.pem -outform PEM
+    faxws_http_code=`curl -s -o /dev/null -w "%{http_code}" -I --cacert /tmp/ssl.pem -u faxws:$FAXWS_TOMCAT_PASSWORD https://faxws/faxWs`
     /health-checker.sh $faxws_http_code "404"
 fi
 
