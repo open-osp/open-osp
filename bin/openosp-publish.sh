a2392c36046d6377e7c6dba27e09d6306b62b360
Clark Van Oyen <clark@countable.ca>
2020-11-09 14:33:27 -0800

    pass command params through

--- bin/openosp-publish.sh
+++ bin/openosp-publish.sh
@@ -2,7 +2,11 @@
 
 set -exo
 
-VER=$(date +"%Y.%m.%d")
+if [[ -z "$3" ]]; then
+  VER=$(date +"%Y.%m.%d")
+else
+  VER=$3
+fi
 docker tag openosp/open-osp:latest openosp/open-osp:$VER
 docker login --username=$DOCKERHUB_USERNAME --password=$DOCKERHUB_PASSWORD
 docker push openosp/open-osp:$VER
