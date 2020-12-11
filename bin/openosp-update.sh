99d0027c638787a6ceb3704bfe67c99466fca582
Clark <clark@countable.ca>
2020-12-10 22:25:43 -0800

    remove ticks

--- bin/openosp-update.sh
+++ bin/openosp-update.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo "Updating OpenOSP image and restarting. This only works if you are set to the `release` tag."
+echo "Updating OpenOSP image and restarting. This only works if you are set to the release tag."
 
 docker pull openosp/open-osp:release
 docker-compose --compatibility up -d oscar
