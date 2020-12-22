fc36e3ac319fadb23ad04ffcfa4806c7a25c4f77
Clark Van Oyen <clark@countable.ca>
2020-12-22 15:12:36 -0800

    8.5 for realz

--- docker/oscar/entrypoint.sh
+++ docker/oscar/entrypoint.sh
@@ -2,4 +2,6 @@
 
 keytool -noprompt -importcert -alias oscar -keystore /usr/lib/jvm/java-8-openjdk-amd64/jre/lib/security/cacerts -storepass changeit -file /usr/local/tomcat/conf/ssl.crt
 
+
+
 exec "$@"
