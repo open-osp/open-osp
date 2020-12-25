1d57ab54007675d1a5f2e5a9c09757e8b029cfed
Your Name <you@example.com>
2020-12-24 23:16:48 -0800

    update key used

--- bin/openosp-backup.sh
+++ bin/openosp-backup.sh
@@ -34,14 +34,13 @@ folder=$(date +%Y%m)
 
 mkdir -p $DUMP_LOCATION
 
-docker exec -t ${site}_db_1 $BACKUP_CMD | gzip > $DUMP_LOCATION/db.sql.gz
-
 clinicname="openosp-$site"
 echo "done backups"
 
 aws="docker run --rm -t -v $HOME/.aws:/root/.aws -v $(pwd):/open-osp amazon/aws-cli"
 
 if [[ $* == *--s3* ]]; then
+    docker exec -t ${site}_db_1 $BACKUP_CMD | gzip > $DUMP_LOCATION/db.sql.gz
     # Remove double quotes, user might input value enclosed in "" in local.env
     BACKUP_BUCKET="${BACKUP_BUCKET//\"}"
     $aws s3 sync /open-osp/volumes s3://$BACKUP_BUCKET/$clinicname/volumes --storage-class STANDARD_IA --exclude ".sync/*"
@@ -49,11 +48,11 @@ if [[ $* == *--s3* ]]; then
 fi
 
 if [[ $* == *--efs* ]]; then
+    docker exec -t ${site}_db_1 $BACKUP_CMD | gzip > $DUMP_LOCATION/db.sql.gz
     mkdir -p /srv/efs/$clinicname/volumes
     mkdir -p /srv/efs/$clinicname/$folder/
     rsync -av ./volumes/ /srv/efs/$clinicname/volumes/
     mv $DUMP_LOCATION/db.sql.gz /srv/efs/$clinicname/$folder/$filename.sql.gz
-
 fi
 
 if [[ $* == *--hdc* ]]; then
@@ -88,7 +87,7 @@ if [[ $* == *--hdc* ]]; then
     
     # encrypt
     # always trust the key, it's verified manually.
-    gpg --output hdc-$filename.sql.gpg --encrypt --trust-model always --recipient pki-dev@hdcbc.ca hdc-$filename.sql
+    gpg --output hdc-$filename.sql.gpg --encrypt --trust-model always --recipient pki-prod@hdcbc.ca hdc-$filename.sql
     #rm hdc-$filename
     
     aws s3 cp hdc-$filename.sql.gpg s3://openosp-hdc-transit/$clinicname/$folder/hdc-$filename.sql.gpg
