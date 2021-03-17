7ca73fee83d83dce2f3b0d8ba7e39d2db761ffd5
Your Name <you@example.com>
2021-03-16 23:30:48 -0700

    health script to properly ignore faxws when disabled

--- docker/health/health.sh
+++ docker/health/health.sh
@@ -2,15 +2,15 @@
 
 set -euxo
 
-ping -c 1 faxws
 ping -c 1 expedius
 ping -c 1 db
 ping -c 1 oscar
 
 
-if [ -z ${FAXWS_TOMCAT_PASSWORD+x} ]; then
+if [ -z $(docker-compose ps | grep faxws) ]; then
     echo "skipping faxws, not defined."
 else
+    ping -c 1 faxws
     faxws_allow=`curl -Ik -u faxws:$FAXWS_TOMCAT_PASSWORD https://faxws/faxWs 2>/dev/null | grep "Allow" | head -1 | cut -d":" -f2`
     openssl x509 -in /usr/local/tomcat/conf/ssl.crt -out /tmp/ssl.pem -outform PEM
     faxws_http_code=`curl -s -o /dev/null -w "%{http_code}" -I --cacert /tmp/ssl.pem -u faxws:$FAXWS_TOMCAT_PASSWORD https://faxws/faxWs`
