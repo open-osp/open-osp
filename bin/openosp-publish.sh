3007ebb87a9b5e5f751c067ead34ab1c0af2fb70
Clark Van Oyen <clark@countable.ca>
2022-01-07 16:13:21 -0800

    publish the release tag

--- bin/openosp-publish.sh
+++ bin/openosp-publish.sh
@@ -7,8 +7,10 @@ if [[ -z $1 ]]; then
 else
   VER=$1
 fi
+docker tag openosp/open-osp:latest openosp/open-osp:release
 docker tag openosp/open-osp:latest openosp/open-osp:$VER
 #docker login --username=$DOCKERHUB_USERNAME --password=$DOCKERHUB_PASSWORD
 docker push openosp/open-osp:$VER
 docker push openosp/open-osp:latest
+docker push openosp/open-osp:release
 
