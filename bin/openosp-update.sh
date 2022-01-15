ac57c5b0fd99d9b81aeead395e6e36130d6c525c
Clark Van Oyen <clark@countable.ca>
2022-01-14 23:23:49 -0800

    updater to include all services

--- bin/openosp-update.sh
+++ bin/openosp-update.sh
@@ -3,5 +3,8 @@
 echo "Updating OpenOSP image and restarting. This only works if you are set to the release tag."
 
 docker pull openosp/open-osp:release
-docker-compose --compatibility up -d oscar
+docker pull openosp/expedius:latest
+docker pull openosp/faxws:latest
+
+docker-compose --compatibility up -d
 
