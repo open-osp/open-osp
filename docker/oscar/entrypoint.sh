5cb28343d2bc4bec8776a2b3f3900102a7655c73
Your Name <you@example.com>
2022-01-17 23:46:13 -0800

    some fixes for release

--- docker/oscar/entrypoint.sh
+++ docker/oscar/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-keytool -noprompt -importcert -alias oscar -keystore /usr/lib/jvm/java-8-openjdk-amd64/jre/lib/security/cacerts -storepass changeit -file /usr/local/tomcat/conf/ssl.crt
+keytool -noprompt -importcert -alias oscar -keystore /usr/local/openjdk-8/lib/security/cacerts -storepass changeit -file /usr/local/tomcat/conf/ssl.crt
 
 
 
