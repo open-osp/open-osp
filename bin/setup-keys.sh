be6d5373180ddca373cbe805f8e365dd5882806e
Fred Ngo <fredngo@gmail.com>
2022-04-26 13:15:09 -0400

    Create an empty chain.pem file. If an empty file doesn't exist, then Docker adds chain.pem as a **directory** instead of a file, which, when bringing up the openosp image, results in:
    "Error response from daemon: error while creating mount source path './volumes/chain.pem': chown ./volumes/chain.pem: operation not permitted"

--- bin/setup-keys.sh
+++ bin/setup-keys.sh
@@ -5,3 +5,10 @@ if [ ! -f ./volumes/ssl.key ] || [ ! -f ./volumes/ssl.crt ] ; then
     openssl req -new -newkey rsa:4096 -days 3650 -nodes -x509 -subj "/C=CA/ST=BC/L=Vancouver/O=OpenOSP/CN=faxws" -keyout ./volumes/ssl.key -out ./volumes/ssl.crt
 fi
 
+# Create an empty chain.pem file
+# If an empty file doesn't exist, then Docker adds chain.pem as a **directory** instead of a file,
+# which results in an "Error response from daemon: error while creating mount source path './volumes/chain.pem': chown ./volumes/chain.pem: operation not permitted"
+# when bringing up the openosp image
+if [ ! -f ./volumes/chain.pem ] ; then
+    touch ./volumes/chain.pem
+fi
