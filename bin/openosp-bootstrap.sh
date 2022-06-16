3b24058047e48152ec14c8d42ddd4dbddc8eb0e9
Fred Ngo <fredngo@gmail.com>
2022-06-16 16:25:52 -0400

    Don't disable TTY
    - disabling TTY can cause a remote server to hang up if running these commands through a tool that communicates with the server via SSH
    - no other docker-compose run commands in this app disables TTY, why does this one?

--- bin/openosp-bootstrap.sh
+++ bin/openosp-bootstrap.sh
@@ -9,8 +9,8 @@ echo "Setting up database..."
 docker-compose up -d db
 sleep 10
 
-docker-compose -f docker-compose.build.yml run --rm -T builder ./bin/clone.sh ${OSCAR_REPO:-""} ${OSCAR_TREEISH:-""}
-docker-compose exec -T db ./bin/populate-db.sh ${LOCATION:-""}
+docker-compose -f docker-compose.build.yml run --rm builder ./bin/clone.sh ${OSCAR_REPO:-""} ${OSCAR_TREEISH:-""}
+docker-compose exec db ./bin/populate-db.sh ${LOCATION:-""}
 
 ./bin/setup-faxws.sh
 
