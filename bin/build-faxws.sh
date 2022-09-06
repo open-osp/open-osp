1408d108e593a5626a3e1ccc62635e898d717279
Fred Ngo <fredngo@gmail.com>
2022-09-06 10:23:16 -0400

    Clean up cloning and building in anticipation of some refactoring down the road

--- bin/build-faxws.sh
+++ bin/build-faxws.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
-cd docker/faxws
+repo_name='faxws'
+repo_url='https://bitbucket.org/openoscar/faxws.git'
+repo_branch='master'
+repo_path='docker/faxws/faxws'
 
-## Clone
-git clone https://bitbucket.org/openoscar/faxws.git
+echo "Cloning ${repo_name} from ${repo_url} to ${repo_path}"
+if [ -d $repo_path ]; then
+  echo "$repo_path already exists. Delete the directory if you want to recompile from scratch."
+else
+  git clone $repo_url $repo_path --branch $repo_branch
+fi
 
-cd faxws
+cd $repo_path
 
 echo "Remove context xml so we can replace it after WAR extraction."
-rm src/main/webapp/META-INF/context.xml
+rm -f src/main/webapp/META-INF/context.xml
 
 echo "Build FaxWs with Maven..."
 
--- bin/build-oscar.sh
+++ bin/build-oscar.sh
@@ -2,8 +2,8 @@
 
 set -uxo
 
-if [ -f "./local.env" ]; then
-    source ./local.env
+if [ -f './local.env' ]; then
+  source ./local.env
 fi
 
 ./bin/clone.sh ${OSCAR_REPO:-""} ${OSCAR_TREEISH:-""}
@@ -17,11 +17,11 @@ export MAVEN_OPTS="-Xms640m -Xmx640m -Xss512k -XX:NewRatio=4 -Djava.net.preferIP
 # this repository should have passed unit testing and mvn verify 
 # on the cis before being built here.
 if [[ "${TEST_DURING_BUILD:-}" ]]; then
-    mvn clean package
+  mvn clean package
 elif [[ "${DEVELOPMENT_MODE:-}" ]]; then
-    mvn -T 1C install --offline
+  mvn -T 1C install --offline
 else
-    mvn -Dcheckstyle.skip -Dmaven.test.skip=true clean package
+  mvn -Dcheckstyle.skip -Dmaven.test.skip=true clean package
 fi
 
 chmod 777 -R ./target/
