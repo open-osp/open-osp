de51a9d672fdf80ec9ecd686b25a6a5c5ef3a507
Colcamex Resources Inc <dwarren@colcamex.com>
2023-06-22 21:38:20 -0700

    bump one more version

--- docker/faxws/entrypoint.sh
+++ docker/faxws/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-keytool -noprompt -importcert -alias oscar -keystore /opt/java/openjdk/lib/security/cacerts -storepass changeit -file /usr/local/tomcat/conf/ssl.crt
+keytool -noprompt -importcert -alias oscar -keystore ${JAVA_HOME}/lib/security/cacerts -storepass changeit -file /usr/local/tomcat/conf/ssl.crt
 
 for filename in $ENVSUBST_CONFIG_FILES; do
     if [ -f $filename.template ]; then
