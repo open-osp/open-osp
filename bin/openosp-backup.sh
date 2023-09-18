73d68cb67ee2b1f33e9e0346fd7e6e0f7e55b887
Colcamex Resources Inc <dwarren@colcamex.com>
2023-09-18 12:00:13 -0700

    outdated script causes S3 backup commands to fail

--- bin/openosp-backup.sh
+++ bin/openosp-backup.sh
@@ -3,12 +3,12 @@
 set -x
 
 # start up Expedius again on any exit of this script.
-trap 'docker compose  start expedius' EXIT
+trap 'docker compose start expedius' EXIT
 
 # Do not execute this script during production hours.
 
 # stop Expedius to avoid connection timeouts with OSCAR during the backup process.
-docker compose  stop expedius
+docker compose stop expedius
 
 if [ -z "$BACKUP_BUCKET" ]
 then
@@ -18,7 +18,7 @@ fi
 
 if [ -z "$BACKUP_CMD" ]
 then
-    BACKUP_CMD="mysqldump -uroot -p${MYSQL_ROOT_PASSWORD} oscar"
+    BACKUP_CMD="mysqldump -u root -p${MYSQL_ROOT_PASSWORD} oscar"
     echo "BACKUP_CMD env var not specified, defaulting to '${BACKUP_CMD}'."
 fi
 
@@ -48,7 +48,7 @@ echo "done backups"
 aws="docker run --rm -t -v $HOME/.aws:/root/.aws -v $(pwd):/open-osp amazon/aws-cli"
 
 if [[ $* == *--s3* ]]; then
-    docker exec -t ${site}_db_1 $BACKUP_CMD | gzip > $DUMP_LOCATION/db.sql.gz
+    docker compose exec -T db $BACKUP_CMD | gzip > $DUMP_LOCATION/db.sql.gz
     # Remove double quotes, user might input value enclosed in "" in local.env
     BACKUP_BUCKET="${BACKUP_BUCKET//\"}"
     $aws s3 sync /open-osp/volumes s3://$BACKUP_BUCKET/$clinicname/volumes --storage-class STANDARD_IA --exclude ".sync/*"
@@ -56,7 +56,7 @@ if [[ $* == *--s3* ]]; then
 fi
 
 if [[ $* == *--efs* ]]; then
-    docker exec -t ${site}_db_1 $BACKUP_CMD | gzip > $DUMP_LOCATION/db.sql.gz
+    docker compose exec -T db $BACKUP_CMD | gzip > $DUMP_LOCATION/db.sql.gz
     mkdir -p /srv/efs/$clinicname/volumes
     mkdir -p /srv/efs/$clinicname/$folder/
     rsync -av ./volumes/ /srv/efs/$clinicname/volumes/
@@ -64,7 +64,7 @@ if [[ $* == *--efs* ]]; then
 fi
 
 if [[ $* == *--hdc* ]]; then
-    docker compose  exec -T db mysqldump -uroot -p${MYSQL_PASSWORD} --skip-triggers oscar \
+    docker compose exec -T db mysqldump -uroot -p${MYSQL_PASSWORD} --skip-triggers oscar \
     allergies \
     appointment \
     appointmentType \
@@ -104,9 +104,9 @@ fi
 
 if [[ $* == *--archive-logs* ]]; then
     ARCHIVE_NAME=log_archive_$(date +"%d-%m-%y").sql
-    docker compose  exec -T db mysqldump -uroot -p${MYSQL_ROOT_PASSWORD} oscar log > $ARCHIVE_NAME
+    docker compose exec -T db mysqldump -uroot -p${MYSQL_ROOT_PASSWORD} oscar log > $ARCHIVE_NAME
     mv $ARCHIVE_NAME volumes/OscarDocument/oscar/
-    docker compose  exec -T db mysql -uroot -p${MYSQL_ROOT_PASSWORD} oscar < bin/archive-logs.sql
+    docker compose exec -T db mysql -uroot -p${MYSQL_ROOT_PASSWORD} oscar < bin/archive-logs.sql
 fi
 
 # restart OSCAR.
