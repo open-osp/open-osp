01e354e13d02b7f79aa3ffd3a4f73da605108594
Colcamex Resources Inc <dwarren@colcamex.com>
2025-01-13 11:31:47 -0800

    change from bitbucket to github - use main branch

--- bin/clone.sh
+++ bin/clone.sh
@@ -3,8 +3,8 @@
 set -uxo
 
 repo_name='oscar'
-repo_url=${1:-https://bitbucket.org/openoscar/oscar.git}
-repo_branch=${2:-release/Open-O-24}
+repo_url=${1:-https://github.com/open-osp/Open-O.git}
+repo_branch=${2:-main}
 repo_path='docker/oscar/oscar'
 
 echo "Cloning ${repo_name} from ${repo_url} to ${repo_path}"
